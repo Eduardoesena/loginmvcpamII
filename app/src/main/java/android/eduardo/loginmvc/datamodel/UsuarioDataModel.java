5ee37cab5a8b3f429c244e3d8c961b51c462ba4b
android <rayane.tahara@etec.sp.gov.br>
2025-08-06 22:10:16 -0300

    Atualização 06-08-24

--- app/src/main/java/android/eduardo/loginmvc/datamodel/UsuarioDataModel.java
+++ app/src/main/java/android/eduardo/loginmvc/datamodel/UsuarioDataModel.java
@@ -1,4 +1,21 @@
 package android.eduardo.loginmvc.datamodel;
 
 public class UsuarioDataModel {
+    public static final String TABELA ="usuario";
+    public static final String ID = "id";
+    public static final String NOME = "nome";
+    public static final String EMAIl = "email";
+    public static final String SENHA = "senha";
+
+    public static  String queryCriarTabela = "";
+    public String criarTabela = "";
+    public String criarTabela(){
+        queryCriarTabela += "CREAT TABELA" + TABELA + "(";
+        queryCriarTabela += ID+ "int primary key autoincrement";
+        queryCriarTabela += NOME + " text,";
+        queryCriarTabela += EMAIl + " text,";
+        queryCriarTabela += SENHA + " text,";
+        queryCriarTabela += ")";
+
+    }
 }
